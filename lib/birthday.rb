5a8ace6d618cd46c5f58c5a0c15b3acfd3dae899
Olivia Cubela <oliviacubela@gmail.com>
2020-06-15 15:08:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if birthday_kids <= 12
+    if birthday_kids <= "12"
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       "Happy Birthday #{kids_name}! You are now #{age} years old. You are too old for this."
